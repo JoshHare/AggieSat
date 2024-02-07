08c78e0465987d9b39af229c807150fbb23d5d4d
christianMosquera <christian.mosquera@me.com>
2024-02-07 16:39:09 +0000

    Login view basic structure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   port = ENV.fetch('PORT') { 3000 }
-  config.action_mailer.default_url_options = { host: 'your-hostname.herokuapp.com', port: port }
+  # config.action_mailer.default_url_options = { host: 'your-hostname.herokuapp.com', port: port }
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
