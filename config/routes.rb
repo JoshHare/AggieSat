ca3a894d3c894699db2c61b73cdb1575b0cf1c0c
root <root@localhost>
2024-02-12 20:49:10 +0000

    feature to display project and workday

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   get '/upload', to: 'pdf_processor#upload', as: 'upload'
   post 'pdf_processor/process_pdf'
 
+  get '/show' , to: 'member_view#show' , as: 'show'
+
+
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
