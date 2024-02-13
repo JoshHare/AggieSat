189a76b66778b3cf86e516dbfa7854c8f9491b70
root <root@localhost>
2024-02-13 01:41:07 +0000

    brakeman fixes

--- app/controllers/member_view_controller.rb
+++ app/controllers/member_view_controller.rb
@@ -1,18 +1,16 @@
 # app/controllers/members_controller.rb
 
 class MemberViewController < ApplicationController
-    before_action :set_member, only: [:show]
-  
-    def show
-      @projects = Project.where(params[:project_name])
-      @scheduled_workdays = ScheduledWorkday.where(params[:day])
-      
-    end
-  
-    private
-  
-    def set_member
-      @member = User.find(current_user.id)
-    end
+  before_action :set_member, only: [:show]
 
+  def show
+    @projects = Project.where(project_name: params[:project_name])
+    @scheduled_workdays = ScheduledWorkday.where(day: params[:day])
   end
+
+  private
+
+  def set_member
+    @member = User.find(current_user.id)
+  end
+end
