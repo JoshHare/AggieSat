b022f4c16fee0ba58839ac8fd5a2478fcd43f9db
root <root@localhost>
2024-02-21 21:36:09 +0000

    added chec validity of training function~c

--- app/controllers/training_enrollments_controller.rb
+++ app/controllers/training_enrollments_controller.rb
@@ -3,10 +3,12 @@
 class TrainingEnrollmentsController < ApplicationController
   def index
     @enrollments = TrainingEnrollment.all
+    @valid_results = @enrollments.map { |enrollment| check_validity(enrollment) }
   end
 
   def show
     @enrollment = TrainingEnrollment.find(params[:id])
+    @valid_result = check_validity(@enrollment)
   end
 
   def new
@@ -50,7 +52,37 @@ class TrainingEnrollmentsController < ApplicationController
 
   private
 
+  def check_validity(enrollment)
+    if out_of_date?(enrollment)
+      "BAD"
+    elsif almost_out_of_date?(enrollment)
+      "WARNING"
+    else
+      "GOOD"
+    end
+  end
+
+
   def enrollment_params
     params.require(:training_enrollment).permit(:course_id, :user_id, :completion_status)
   end
+
+  def out_of_date?(enrollment) #checks if the training is expired (1 year old)
+    if enrollment.completion_status.present?
+      completion_status_threshold = 1.year.ago
+      return enrollment.completion_status < completion_status_threshold
+    end
+
+    false
+  end
+
+  def almost_out_of_date?(enrollment)
+    if enrollment.completion_status.present?
+      completion_status_threshold = 11.months.ago
+      return enrollment.completion_status < completion_status_threshold
+    end
+
+    false
+  end
+
 end
