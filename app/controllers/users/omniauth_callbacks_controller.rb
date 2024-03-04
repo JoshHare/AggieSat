69883c506557c0c4ba5d4170e355f726fbd866ab
Lucy Zhang <lululilly1212@tamu.edu>
2024-03-04 04:32:00 +0000

    greater coverage and test cases

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,7 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         sign_out_all_scopes
         flash[:success] = t('devise.omniauth_callbacks.success', kind: 'Google')
         sign_in_and_redirect(user, event: :authentication)
-      else
+      else 
         flash[:alert] = t('devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} had issues saving.")
         redirect_to(new_user_session_path)
       end
@@ -23,7 +23,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   protected
 
   def after_omniauth_failure_path_for(_scope)
-    new_user_session_path
+    new_user_session_path(_scope)
   end
 
   def after_sign_in_path_for(resource_or_scope)
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -17,14 +17,75 @@ require 'rails_helper'
    # expect(page).to have_content('harry potter')
   #end
 #end
-RSpec.describe 'Navigating through the default member view like a user: ', type: :feature do
+
+RSpec.describe 'Checking security/integrity of application with google oauth: ', type: :feature do
 
   before do
     Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
   end
 
-  scenario 'authentication success' do #, :js => true, :driver => :rack_test do
+  scenario 'authentication success' do #successfully log in with mockauth user
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    visit test_path
+    expect(page).to have_content('Welcome')
+  end
+
+  scenario 'authentication fail due to not being in scope of org' do #unsuccessful login due to email not in org scope
+    OmniAuth.config.add_mock(:google_oauth2, {
+      :info => {:email => 'bloblo@tamu.edu',
+      :full_name => 'blob blob',
+      :avatar_url => 'testing'},
+    })
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    expect(page).to have_content('email')
+  end 
+
+  #not a tamu email login does not work because mockauth does not hit google's endpoint
+
+  scenario 'invalid login information' do #unsuccessful login due to login error
+    OmniAuth.config.add_mock(:google_oauth2, {
+      :info => {:email => 'amybob@tamu.edu',
+      :full_name => '',
+      :avatar_url => ''},
+    })
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    expect(page).to have_content('issues saving')
+  end 
+  
+end
+
+RSpec.describe 'Checking usability of website: ', type: :feature do
+
+  before do
+    Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    OmniAuth.config.add_mock(:google_oauth2, {
+      :info => {:email => 'amybob@tamu.edu',
+      :full_name => 'amy bob',
+      :avatar_url => 'testing'},
+    })
+  end
+
+  scenario 'all paths accessible after log in as member' do #check usability as member
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    visit test_path
+    expect(page).to have_content('Welcome')
+    visit upload_path
+    expect(page).to have_content('Upload PDF')
+    visit show_path
+    expect(page).to have_content('Workday')
+  end
+
+  scenario 'all paths accessible after log in as member' do #check usability as project lead
     visit new_user_session_path
     click_on 'Sign in with Google'
 
@@ -38,3 +99,64 @@ RSpec.describe 'Navigating through the default member view like a user: ', type:
 
 end
 
+RSpec.describe 'PDF proccessor integration test: ', type: :feature do
+
+  before do
+    Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    OmniAuth.config.add_mock(:google_oauth2, {
+      :info => {:email => 'amybob@tamu.edu',
+      :full_name => '',
+      :avatar_url => ''},
+    })
+  end
+
+  scenario 'upload valid document from traintraq' do #check usability as member
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    visit upload_path
+    attach_file "pdf", "spec/test_files/validTrainingTT.pdf"
+    click_button "Process PDF"
+    expect(page).to have_content('successfully uploaded')
+    expect(page).to have_content('Kate E Woodard 2112861 8/29/2023')
+
+  end
+
+  scenario 'upload valid document from as combined pdf' do #check usability as member
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    visit upload_path
+    attach_file "pdf", "spec/test_files/validTrainingC.pdf"
+    click_button "Process PDF"
+    expect(page).to have_content('successfully uploaded')
+    expect(page).to have_content('Pan Zhou 2112861 9/1/2023')
+
+  end
+
+  scenario 'upload expired training' do #expired trianing
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    visit test_path
+    expect(page).to have_content('Welcome')
+    visit upload_path
+    expect(page).to have_content('Upload PDF')
+    visit show_path
+    expect(page).to have_content('Workday')
+  end
+
+  scenario 'upload invalid file format' do #invalid file
+    visit new_user_session_path
+    click_on 'Sign in with Google'
+
+    visit test_path
+    expect(page).to have_content('Welcome')
+    visit upload_path
+    expect(page).to have_content('Upload PDF')
+    visit show_path
+    expect(page).to have_content('Workday')
+  end
+
+end
